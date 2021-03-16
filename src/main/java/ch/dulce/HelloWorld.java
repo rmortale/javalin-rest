bf0cf1d8152d5f2e681363f2cdf4f2ec70ff0500
Antonio Dolce <dolce.antonio@gmail.com>
2021-03-16 18:26:38 +0100

    added interface to bind to

--- src/main/java/ch/dulce/HelloWorld.java
+++ src/main/java/ch/dulce/HelloWorld.java
@@ -5,7 +5,7 @@ import io.javalin.Javalin;
 
 public class HelloWorld {
   public static void main(String[] args) {
-    Javalin app = Javalin.create().start(7000);
+    Javalin app = Javalin.create().start("0.0.0.0", 7000);
     app.get("/", ctx -> ctx.result("Hello World"));
   }
 }
